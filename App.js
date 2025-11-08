c54e39163fa11f86e9711d401f12cb16e4903bb2
RakeshNayak <n.rakesh0023@gmail.com>
2025-11-08 15:48:11 +0530

    Ep-2.1 starts

---
+++ App.js
@@ -0,0 +1,16 @@
+const parent =React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{},"hiii im h1 tag"),
+        React.createElement("h2",{},"hiii im h2 tag")
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"hiii im h1 tag"),
+        React.createElement("h2",{},"hiii im h2 tag")
+    ])
+]);
+
+console.log(parent);
+ 
+const root=ReactDOM.createRoot(document.getElementById("root"));
+//root.render(heading);
+root.render(parent);
